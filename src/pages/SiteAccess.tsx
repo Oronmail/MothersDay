6e61afbd4c5977b741c875aa3bee014d00cfbbdc
Oron Mizrachi <oronsmac@Orons-MacBook-Air-2.local>
2026-04-13 15:10:22 +0300

    fix: remove password gate from storefront

--- src/pages/SiteAccess.tsx
+++ src/pages/SiteAccess.tsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
-import PasswordGate from "@/components/PasswordGate";
 import { NewsletterPopup } from "@/components/NewsletterPopup";
 
 const SiteAccess = () => {
   return (
-    <PasswordGate>
+    <>
       <NewsletterPopup />
       <Outlet />
-    </PasswordGate>
+    </>
   );
 };
 
